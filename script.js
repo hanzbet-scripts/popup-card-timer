38cb67227ee2ed51620fd998e99d1565e7490f2b
thosijulio <thosijulio@gmail.com>
2025-05-26 16:28:46 -0300

    teste

--- script.js
+++ script.js
@@ -3,18 +3,6 @@
 // meta.content = '"width=device-width, initial-scale=1.0"';
 // document.head.appendChild(meta);
 
-const buttonPlay = document.getElementById("button-play");
-const buttonClose = document.getElementById("thosijulio-close");
-
-buttonPlay.addEventListener("click", (ev) => {
-  ev.preventDefault();
-  window.location.replace("https://google.com");
-});
-
-buttonClose.addEventListener("click", () => {
-  window.location.replace("https://facebook.com");
-});
-
 const link = document.createElement("link");
 link.href =
   "https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;700;800&display=swap";
