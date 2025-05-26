6e7845e1104a4288c0e77eff758c937920dc081f
thosijulio <thosijulio@gmail.com>
2025-05-26 17:05:46 -0300

    FEAT - timer

--- timer.js
+++ timer.js
@@ -1,48 +1,60 @@
-function timer() {
-  try {
-    const element = document.getElementById("thosijulio-timer");
+let elementExist = false;
 
-    function updateTimer() {
-      const now = new Date();
-      const endOfDay = new Date();
-      endOfDay.setHours(23, 59, 59, 999);
+while (!elementExist) {
+  setTimeout(() => {
+    const element = document.getElementById('thosijulio-timer');
 
-      const diff = endOfDay - now;
-
-      if (diff <= 0) {
-        element?.innerHTML = "O tempo acabou.";
-        clearInterval(interval);
-        return;
+    if (element) {
+      elementExist = true;
+      function timer() {
+        try {
+          const element = document.getElementById("thosijulio-timer");
+      
+          function updateTimer() {
+            const now = new Date();
+            const endOfDay = new Date();
+            endOfDay.setHours(23, 59, 59, 999);
+      
+            const diff = endOfDay - now;
+      
+            if (diff <= 0) {
+              element?.innerHTML = "O tempo acabou.";
+              clearInterval(interval);
+              return;
+            }
+      
+            const hours = Math.floor(diff / (1000 * 60 * 60));
+            const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+      
+            alert(seconds);
+            element?.inqnerHTML =
+              `${hours.toString().padStart(2, "0")}h` +
+              `${minutes.toString().padStart(2, "0")}m` +
+              `${seconds.toString().padStart(2, "0")}s`;
+          }
+      
+          updateTimer(); // atualiza imediatamente ao carregar
+          const interval = setInterval(updateTimer, 1000); // atualiza a cada segundo
+      
+          const buttonPlay = document.getElementById("button-play");
+          const buttonClose = document.getElementById("thosijulio-close");
+      
+          buttonPlay?.addEventListener("click", (ev) => {
+            ev.preventDefault();
+            window.location.replace("https://google.com");
+          });
+      
+          buttonClose?.addEventListener("click", () => {
+            window.location.replace("https://facebook.com");
+          });
+        } catch (error) {
+          alert("erro: " + error.message);
+        }
       }
-
-      const hours = Math.floor(diff / (1000 * 60 * 60));
-      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-      alert(seconds);
-      element?.inqnerHTML =
-        `${hours.toString().padStart(2, "0")}h` +
-        `${minutes.toString().padStart(2, "0")}m` +
-        `${seconds.toString().padStart(2, "0")}s`;
+      
+      timer();
     }
-
-    updateTimer(); // atualiza imediatamente ao carregar
-    const interval = setInterval(updateTimer, 1000); // atualiza a cada segundo
-
-    const buttonPlay = document.getElementById("button-play");
-    const buttonClose = document.getElementById("thosijulio-close");
-
-    buttonPlay?.addEventListener("click", (ev) => {
-      ev.preventDefault();
-      window.location.replace("https://google.com");
-    });
-
-    buttonClose?.addEventListener("click", () => {
-      window.location.replace("https://facebook.com");
-    });
-  } catch (error) {
-    alert("erro: " + error.message);
-  }
+  }, 500)
 }
 
-timer();
