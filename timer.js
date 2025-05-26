76aec5010865cf5f6aea577983b517d1a646c88d
thosijulio <thosijulio@gmail.com>
2025-05-26 16:34:46 -0300

    FEAT - timer

--- timer.js
+++ timer.js
@@ -18,6 +18,7 @@ function timer() {
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
+    alert(seconds);
     element.innerHTML =
       `${hours.toString().padStart(2, "0")}h` +
       `${minutes.toString().padStart(2, "0")}m` +
